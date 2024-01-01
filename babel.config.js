3b73d5fc00f10466adad184dc889da97a697de43
Bogdan Kolesov <bogdan_kolesov@bk.ru>
2024-01-01 17:08:32 +0600

    fix: Home

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
+
+  const presets = ['babel-preset-expo'];
+  const plugins = [
+    [
+      'module:react-native-dotenv',
+      {
+        'envName': 'APP_ENV',
+        'moduleName': '@env',
+        'path': '.env',
+      },
+    ],
+  ];
+
   return {
-    presets: ['babel-preset-expo'],
+    presets,
+    plugins,
   };
 };
--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -1,8 +1,31 @@
-import {SafeAreaView, StatusBar, View, Text, Image, TextInput} from 'react-native'
-import React from 'react'
-import Ionic from 'react-native-vector-icons/Ionicons'
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StatusBar, View, Text, Image, TextInput, FlatList } from 'react-native';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
+import { APP_BASE_URL, APP_API_KEY } from '@env';
 
 export default function Home() {
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    getMoviesDataFromDB();
+  }, []);
+
+  const getMoviesDataFromDB = async () => {
+    try {
+      const response = await axios.get(`${APP_BASE_URL}/movie/popular?language=ru_RU&page=1`, {
+        headers: {
+          Authorization: `Bearer ${APP_API_KEY}`
+        }
+      });
+
+      setMoviesData(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(moviesData);
   return (
     <SafeAreaView style={{width: '100%', height: '100%', backgroundColor: '#1A1A23', padding: 0, position: 'relative'}}>
       <StatusBar barStyle='light-content' backgroundColor='#1A1A23'/>
@@ -44,6 +67,38 @@ export default function Home() {
       </View>        
       </View>
       </View>
+      <View 
+      style={{
+        flexDirection:'row',
+        padding: 20,
+        marginBottom: 20,
+        marginTop: 10
+      }}> 
+        <Text style={{
+          color: '#ffffff',
+          fontSize: 16,
+          marginRight: 8,
+          fontWeight:'bold'
+        }}>
+          Фильмы
+        </Text>
+        <Text style={{
+          color: '#ffffff',
+          fontSize: 16,
+          marginRight: 8
+        }}>
+        в прокате
+        </Text>
+      </View>
+      <FlatList
+        data={moviesData}
+        renderItem={({ item, index }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   )
 }
